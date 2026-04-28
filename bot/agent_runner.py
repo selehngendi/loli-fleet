7c8147d5b3131e9cae7e2abb15fddce8308cf6a7
selehngendi <selehngendi@users.noreply.github.com>
2026-04-28 21:53:43 +0700

    Update default agent name to lolali

--- bot/agent_runner.py
+++ bot/agent_runner.py
@@ -91,7 +91,7 @@ class AgentRunner:
         agent_addr= self._env("WALLET_ADDRESS")
         owner_pk  = self._env("OWNER_KEY")
         owner_addr= self._env("OWNER_EOA")
-        name      = self._env("NAME", f"MoltyAgent{self.index}")
+        name      = self._env("NAME", f"lolali-{self.index:02d}")
 
         if api_key and agent_pk:
             self.log.info("♻️  Restoring from env vars (agent=%s)", name)
